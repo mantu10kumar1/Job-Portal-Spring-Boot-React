cd198bc9095e213f48e5dbe8090082ac32b8a0e5
mantu10kumar1 <mantukumar4376@gmail.com>
2025-07-21 09:49:32 +0530

    I have written some part of the backend like api , service and entity

--- Job-Portal/src/main/java/com/jobportal/JobPortalApplication.java
+++ Job-Portal/src/main/java/com/jobportal/JobPortalApplication.java
@@ -8,7 +8,7 @@ public class JobPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobPortalApplication.class, args);
-		System.out.println("Hello World..");
+		System.out.println("\n\t\t\t\t\t\tJobPortal Application is running...\n");
 	}
 
 }
